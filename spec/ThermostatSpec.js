6f51ab01363c47aa498bbadd105d84df87f2aecc
Makers Laptop 22 <makerslaptop22@Makerss-MacBook-Pro-3.local>
2015-08-18 11:35:54 +0100

    Added power save on function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,20 +5,35 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  describe('has a temperature', function(){
-    it('default is 20', function(){
+  describe('has a temperature', function() {
+    it('default is 20', function() {
         expect(thermostat.temperature()).toEqual(20);
     });
 
-    describe('increases the temperature', function(){
-      it('can increase the temp by 1', function(){
+    describe('increases the temperature', function() {
+      it('can increase the temp by 1', function() {
         expect(thermostat.increase()).toEqual(21);
       });
     });
 
-    describe('decreases the temperate', function(){
-      it('can descrease the temp by 1', function(){
+    describe('decreases the temperate', function() {
+      it('can descrease the temp by 1', function() {
         expect(thermostat.decrease()).toEqual(19);
+      });
+    });
+
+    describe('has a minimum temperature', function() {
+      it('set to 10', function() {
+        for(i=20; i>10; i--){thermostat.decrease()};
+        expect(thermostat.decrease()).toEqual(10);
+      });
+    });
+
+    describe('has a power save button', function() {
+      it('set to On, maximum temperature is set to 25', function() {
+        thermostat.powerSaveOn();
+        for(i=20; i<25; i++){thermostat.increase()};
+        expect(thermostat.increase()).toEqual(25);
       });
     });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,7 @@
 var Thermostat = function(){
   this.currentTemperature = 20;
+  this.maximumTemperature = 25;
+  this.minimumTemperature = 10;
 };
 
 Thermostat.prototype.temperature = function() {
@@ -7,9 +9,19 @@ Thermostat.prototype.temperature = function() {
 };
 
 Thermostat.prototype.increase = function() {
-  return this.currentTemperature+=1;
+  if (this.currentTemperature < this.maximumTemperature){
+    return this.currentTemperature+=1;
+  }
+  return this.currentTemperature;
 };
 
 Thermostat.prototype.decrease = function() {
+  if (this.currentTemperature === this.minimumTemperature){
+    return this.currentTemperature;
+  }
   return this.currentTemperature-=1;
+};
+
+Thermostat.prototype.powerSaveOn = function() {
+  this.maximumTemperature = 25;
 };
